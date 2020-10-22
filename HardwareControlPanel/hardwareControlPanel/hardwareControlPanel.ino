7e13cebb00eab1d9fff180a76df4c19527c65c5b
TheCreamPinkPony <johan.strandberg@gmx.de>
2020-10-22 22:11:57 +0200

    main code for esp32 controlling the hardware control panel

---
+++ HardwareControlPanel/hardwareControlPanel/hardwareControlPanel.ino
@@ -0,0 +1,21 @@
+#define pWakeUp = 14
+#define pHomeUp = 4
+#define pHomeDown = 5
+#define pGuestUp = 34
+#define pGuestDown = 35
+#define pPushToServer = 12
+#define pFeature0 = 2
+#define pFeature1 = 27
+#define pFeature2 = 32
+#define pFeature3 = 33
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
